10836e095f8bf0dd221675e83e2a374070df4183
Eduardo Caceres <eduardo.caceresdelacalle@gmail.com>
2020-03-28 23:45:58 +0100

    Add AWS Lambda CloudFormation PoC

---
+++ Functions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
+using System.Text;
+using System.Net.Http;
+
+namespace TestLambda
+{
+    public class Functions
+    {
+        /// <summary>
+        /// The main entry point for the custom runtime.
+        /// </summary>
+        /// <param name="args"></param>
+        private static async Task Main(string[] args)
+        {
+            Func<string, ILambdaContext, Task<string>> func = FunctionHandler;
+            using var handlerWrapper = HandlerWrapper.GetHandlerWrapper(func, new JsonSerializer());
+            using var bootstrap = new LambdaBootstrap(handlerWrapper);
+            await bootstrap.RunAsync();
+        }
+
+        /// <summary>
+        /// A simple function that takes a string, does a ToUpper of the input
+        /// and appends some random info from https://openlibrary.org
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static async Task<string> FunctionHandler(string input, ILambdaContext context)
+        {
+            var sb = new StringBuilder(input?.ToUpperInvariant());
+            sb.Append("   ");
+
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://openlibrary.org/api/books?bibkeys=OLID:OL13998706M");
+            sb.Append(await result.Content.ReadAsStringAsync());
+
+            return sb.ToString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# AWS_Lambda_CloudFormation_PoC
-AWS Lambda PoC (deployed using CloudFormation) using .NET Core 3.0
+# AWS Lambda CloudFormation PoC
+
+## Requirements
+
+An existing s3 bucket (named `test-lambda-artifact` in this example)
+
+## Instructions
+
+Install or update Amazon.Lambda.Tools Global Tools
+
+```bash
+    dotnet tool install -g Amazon.Lambda.Tools
+    dotnet tool update -g Amazon.Lambda.Tools
+```
+
+Deploy the serverless application
+
+```bash
+    dotnet lambda deploy-serverless --template-file serverless.template --s3-bucket test-lambda-artifact --stack-name testLambdaStack
+```
+
+Test the lambda function
+
+```bash
+    dotnet lambda invoke-function -p "Hey!"
+```
+
+Cleanup
+
+```bash
+    dotnet lambda delete-serverless testLambdaStack
+```
